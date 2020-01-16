7bcd81f3ac453d7af7823958596a23011028e3df
stuffy187 <ehuckaby1488@icloud.com>
2020-01-15 23:03:11 -0500

    Delete each.rb

--- spec/cases/each.rb
+++
@@ -1,8 +0,0 @@
-require './spec/cases/helper'
-STDOUT.sync = true # otherwise results can go weird...
-
-x = ['a','b','c','d']
-result = Parallel.each(x) do |x|
-  sleep 0.1 if x == 'a'
-end
-print result * ' '
