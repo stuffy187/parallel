03158e749a91de0dfe5a9398e1c97b11f6d72e15
stuffy187 <ehuckaby1488@icloud.com>
2020-01-15 22:54:47 -0500

    Delete parallel_map_complex_objects.rb

--- spec/cases/parallel_map_complex_objects.rb
+++
@@ -1,8 +0,0 @@
-require './spec/cases/helper'
-
-object = ["\nasd#{File.read('Gemfile')}--#{File.read('Rakefile')}"*100, 12345, {:b=>:a}]
-
-result = Parallel.map([1,2]) do |x|
-  object
-end
-print 'YES' if result.inspect == [object, object].inspect
